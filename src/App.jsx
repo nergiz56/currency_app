a6b67825f9c8065dbb8d4cf7bc3c206d22533cf8
Nergiz Şengün <nergizduygu56@gmail.com>
2025-03-14 16:42:46 +0300

    iyleştirme

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Currency from './components/Currency'
 function App() {
 
   return (
-    <div style={{display:'flex', flexDirection:'column', justifyItems:"center", alignItems:'center'}}>
+    <div style={{display:'flex', flexDirection:'column', justifyContent:"center", alignItems:'center'}}>
     <Currency/>
     </div>
   )
--- src/components/Currency.jsx
+++ src/components/Currency.jsx
@@ -2,26 +2,40 @@ import React from "react";
 import '../css/currency.css';
 import { FaLongArrowAltRight } from "react-icons/fa";
 
-
-function Currency(){
+function Currency() {
     return (
         <div className='currency-div'>
-            <input type="number" className="amount"/>
-            <select className="from-currency-option">
-            <option>USD</option>
-            <option>EUR</option>
-            <option>TL</option>
-            </select>
-            <FaLongArrowAltRight style={{color: '#fff', fontSize:'30px', marginRight: "10px"}} />
+            <div>
+                <h3 className="currency-title">
+                    DÖVİZ KURU UYGULAMASI
+                </h3>
+            </div>
+            
+            <div className="currency-input-row">
+                <input type="number" className="amount" />
 
-            <select className="to-currency-option">
-            <option>USD</option>
-            <option>EUR</option>
-            <option>TL</option>
-            </select>
-            <input type="number" className="result" />
+                <select className="from-currency-option">
+                    <option>USD</option>
+                    <option>EUR</option>
+                    <option>TL</option>
+                </select>
+
+                <FaLongArrowAltRight className="arrow-icon" />
+
+                <select className="to-currency-option">
+                    <option>USD</option>
+                    <option>EUR</option>
+                    <option>TL</option>
+                </select>
+
+                <input type="number" className="result" />
+            </div>
+
+            <div>
+                <button className="exchange-button">Çevir</button>
+            </div>
         </div>
-    )
+    );
 }
 
-export default Currency
+export default Currency;
